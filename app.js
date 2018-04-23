9a93fdc3b9b8b5727ed243c17621829580742a6b
Rich von Rauchhaupt <Rich@Snow.laptop>
2018-04-23 15:40:11 -0400

    updated packages

--- app.js
+++ app.js
@@ -30,25 +30,25 @@ app.use('/', index);
 app.use('/events/', events);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+function errorHandler (err, req, res, next) {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send({
-    message: err.message,
-    error: err
-  });
+  console.log(err);
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.send({ error: err })
+
   return;
-});
+}
+
+app.use(errorHandler)
 
 module.exports = app;
--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -74,5 +74,6 @@ exports.findAllMeetupEventsFullCal = function(req, res) {
   }).then((calList) => {
     res.json(calList);
   });
+  //.catch(next);
 
 }
